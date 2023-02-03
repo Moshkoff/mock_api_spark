124318a1f50677b68a0c3fe4b7d0f677ed1ff012
Evgeny Moshkoff <evgeny.moshkoff@gmail.com>
2023-02-03 10:23:51 +0300

    added method status

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,14 @@
 class ApiController < ApplicationController
+
+  def status
+    inn = params[:inn]
+
+    random_result= [active, inactive].sample
+
+    @message = {
+      "inn" => inn,
+      "result" => random_result
+    }
+  end
+
 end
